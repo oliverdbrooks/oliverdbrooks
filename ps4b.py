af73596ae42e3d1ef75cc40c9c32d7e4947e06e2
oliverdbrooks <146066126+oliverdbrooks@users.noreply.github.com>
2023-10-15 11:36:32 +0100

    Add files via upload

--- ps4b.py
+++ ps4b.py
@@ -124,6 +124,33 @@ def playGame(wordList):
 
     wordList: list (string)
     """
+    
+    hand = None
+    while True:
+        user_input = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if user_input == 'e':
+            break
+        elif user_input == 'n':
+            hand = dealHand(HAND_SIZE)
+        elif user_input == 'r':
+            if hand is None:
+                print("You have not played a hand yet. Please play a new hand first!")
+                continue
+        else:
+            print("Invalid command.")
+            continue
+        while True:
+            user_input = input("Enter u to have yourself play, c to have the computer play: ")
+            if user_input == 'u':
+                playHand(hand, wordList, HAND_SIZE)
+                break
+            elif user_input == 'c':
+                print("Hand passed to compPlayHand: ", end="")
+                compPlayHand(hand, wordList, HAND_SIZE)
+                break
+            else:
+                print("Invalid command.")
+    
     # TO DO... <-- Remove this comment when you code this function
     print("playGame not yet implemented.") # <-- Remove this when you code this function
 
